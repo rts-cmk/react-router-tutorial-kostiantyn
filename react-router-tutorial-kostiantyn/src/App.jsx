4c6e5e0b5961230debb9fbdc43ae3097e2021957
Overlordka <kopary787@gmail.com>
2025-10-02 09:35:42 +0200

    starting install react router

---
+++ react-router-tutorial-kostiantyn/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <h1>Home Padge</h1>
+  )
+}
+
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
