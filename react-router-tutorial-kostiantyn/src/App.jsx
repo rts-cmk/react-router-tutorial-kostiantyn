13870de4d0ae14bf877c9e527c65150597563ddf
Overlordka <kopary787@gmail.com>
2025-10-02 12:19:42 +0200

    Brugt brouser router, routes, route, link, useNavigate

--- react-router-tutorial-kostiantyn/src/App.jsx
+++ react-router-tutorial-kostiantyn/src/App.jsx
@@ -1,8 +1,39 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate()
+
+  const goToContact = () => {
+    navigate("/contact")
+  }
+
   return (
-    <h1>Home Padge</h1>
+    <div>
+      <h1>Hjem Side</h1>
+      <button onClick={goToContact}>Gå til kontakt</button>
+    </div>
+  )
+}
+
+function About() {
+  return(
+    <h1>Om os side</h1>
+  )
+}
+
+function Contact() {
+  return(
+    <h1>Kontakt side</h1>
+  )
+}
+
+function Navbar() {
+  return(
+    <nav>
+      <Link to="/">Hjem</Link>
+      <Link to="/about">Om os</Link>
+      <Link to="/contact">Kontakt</Link>
+    </nav>
   )
 }
 
@@ -10,9 +41,14 @@ function Home() {
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
